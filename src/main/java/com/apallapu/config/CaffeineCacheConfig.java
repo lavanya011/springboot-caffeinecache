8be02be07d590969cbbe87ed979df2172086a588
Apallapu <43247702+Apallapu@users.noreply.github.com>
2019-08-01 10:08:07 +0530

    done
    done

--- src/main/java/com/apallapu/config/CaffeineCacheConfig.java
+++ src/main/java/com/apallapu/config/CaffeineCacheConfig.java
@@ -1,27 +1,47 @@
 package com.apallapu.config;
 
-import com.github.benmanes.caffeine.cache.Caffeine;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.concurrent.TimeUnit;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.CaffeineSpec;
+import com.github.benmanes.caffeine.cache.RemovalCause;
+import com.github.benmanes.caffeine.cache.RemovalListener;
 
 @Configuration
 public class CaffeineCacheConfig {
 
-    public CacheManager cacheManager(){
-        CaffeineCacheManager cacheManager = new CaffeineCacheManager("customer","account");
-        cacheManager.setCaffeine(caffeineCacheBuilder());
-        return cacheManager;
-    }
+	@Bean
+	public CacheManager cacheManager() {
+		//String specAsString = "initialCapacity=100,maximumSize=500,expireAfterAccess=5m,recordStats";
+		CaffeineCacheManager cacheManager = new CaffeineCacheManager("customer", "account");
+		cacheManager.setAllowNullValues(false); // can happen if you get a value from a @Cachable that returns null
+		// cacheManager.setCacheSpecification(specAsString);
+		// cacheManager.setCaffeineSpec(caffeineSpec());
+		cacheManager.setCaffeine(caffeineCacheBuilder());
+		return cacheManager;
+	}
 
-    Caffeine<Object, Object> caffeineCacheBuilder() {
-        return Caffeine.newBuilder()
-                .initialCapacity(100)
-                .maximumSize(500)
-                .expireAfterAccess(10, TimeUnit.MINUTES)
-                .weakKeys()
-                .recordStats();
-    }
+	CaffeineSpec caffeineSpec() {
+		return CaffeineSpec.parse("initialCapacity=100,maximumSize=500,expireAfterAccess=5m,recordStats");
+	}
+
+	Caffeine<Object, Object> caffeineCacheBuilder() {
+		return Caffeine.newBuilder().initialCapacity(100).maximumSize(150).expireAfterAccess(5, TimeUnit.MINUTES)
+				.weakKeys().removalListener(new CustomRemovalListener()).recordStats();
+	}
+
+	class CustomRemovalListener implements RemovalListener<Object, Object> {
+		@Override
+		public void onRemoval(Object key, Object value, RemovalCause cause) {
+			System.out.format("removal listerner called with key [%s], cause [%s], evicted [%S]\n", key,
+					cause.toString(), cause.wasEvicted());
+		}
+
+	}
+
 }
